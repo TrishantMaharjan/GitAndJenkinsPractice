1670a6c4ad151fb1c94296658ef1ed193e010081
Trishant Maharjan <87901255+TrishantMaharjan@users.noreply.github.com>
2023-08-01 12:06:44 +0545

    Update Area.java

--- JavaPractice/Area.java
+++ JavaPractice/Area.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Area {
 	public static void main(String[] args) {
-		int choice;
+		int choice=1;
 		float area=0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1. Area of circle ");
@@ -10,13 +10,13 @@ public class Area {
 		System.out.println("3. Area of rectangle ");
 		choice = sc.nextInt();
 		if (choice == 1) {
-			float r, pi = 22/7f;
+			float r=7, pi = 22/7f;
 			System.out.println("Enter Radius ");
 			r = sc.nextFloat();
 			area = (pi*r*r);
 		}
 		if (choice == 2) {
-			float b, h;
+			float b=2, h=3;
 			System.out.println("Enter Breadth ");
 			b = sc.nextFloat();
 			System.out.println("Enter Height ");
@@ -24,7 +24,7 @@ public class Area {
 			area = (b*h)/2;
 		}
 		if (choice == 3) {
-			float l, b;
+			float l=4, b=4;
 			System.out.println("Enter Length ");
 			l = sc.nextFloat();
 			System.out.println("Enter Breadth ");

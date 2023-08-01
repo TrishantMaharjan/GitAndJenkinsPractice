4b98371afc6dd318f0539a65a1b7c7ae340c9748
Trishant Maharjan <87901255+TrishantMaharjan@users.noreply.github.com>
2023-08-01 12:12:52 +0545

    Update Area.java with try catch

--- JavaPractice/Area.java
+++ JavaPractice/Area.java
@@ -2,33 +2,51 @@ import java.util.Scanner;
 
 public class Area {
 	public static void main(String[] args) {
-		int choice=1;
+		int choice;
 		float area=0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1. Area of circle ");
 		System.out.println("2. Area of rt angled triangle ");
 		System.out.println("3. Area of rectangle ");
-		choice = sc.nextInt();
+		try {
+      			choice = sc.nextInt();
+    		} catch (Exception e) {
+      			choice=1;
+    		}
 		if (choice == 1) {
 			float r=7, pi = 22/7f;
 			System.out.println("Enter Radius ");
-			r = sc.nextFloat();
+			try {
+      				r = sc.nextFloat();
+    			} catch (Exception e) {
+      				r=7;
+    			}
 			area = (pi*r*r);
 		}
 		if (choice == 2) {
-			float b=2, h=3;
-			System.out.println("Enter Breadth ");
-			b = sc.nextFloat();
-			System.out.println("Enter Height ");
-			h = sc.nextFloat();
+			float b, h;
+			try {
+      				System.out.println("Enter Breadth ");
+				b = sc.nextFloat();
+				System.out.println("Enter Height ");
+				h = sc.nextFloat();
+    			} catch (Exception e) {
+      				b=7;
+				h=9;
+    			}
 			area = (b*h)/2;
 		}
 		if (choice == 3) {
-			float l=4, b=4;
-			System.out.println("Enter Length ");
-			l = sc.nextFloat();
-			System.out.println("Enter Breadth ");
-			b = sc.nextFloat();
+			float l, b;
+			try {
+      				System.out.println("Enter Length ");
+				l = sc.nextFloat();
+				System.out.println("Enter Breadth ");
+				b = sc.nextFloat();
+    			} catch (Exception e) {
+      				b=7;
+				l=9;
+    			}
 			area = (b*l);
 		}
 		System.out.println("Area = "+area);

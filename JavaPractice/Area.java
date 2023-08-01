a83234ded2d97bd7e4a14a29389541dec09b436c
Trishant Maharjan <87901255+TrishantMaharjan@users.noreply.github.com>
2023-08-01 12:20:17 +0545

    Update Area.java added random to other variables

--- JavaPractice/Area.java
+++ JavaPractice/Area.java
@@ -6,22 +6,22 @@ public class Area {
 		int choice;
 		float area=0;
 		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
 		System.out.println("1. Area of circle ");
 		System.out.println("2. Area of rt angled triangle ");
 		System.out.println("3. Area of rectangle ");
 		try {
       			choice = sc.nextInt();
     		} catch (Exception e) {
-			Random random = new Random();
       			choice=random.nextInt(3 - 1 + 1) + 1;
     		}
 		if (choice == 1) {
-			float r=7, pi = 22/7f;
+			float r, pi = 22/7f;
 			System.out.println("Enter Radius ");
 			try {
       				r = sc.nextFloat();
     			} catch (Exception e) {
-      				r=7;
+      				r=random.nextInt(90 - 1 + 1) + 1;
     			}
 			area = (pi*r*r);
 		}
@@ -33,8 +33,8 @@ public class Area {
 				System.out.println("Enter Height ");
 				h = sc.nextFloat();
     			} catch (Exception e) {
-      				b=7;
-				h=9;
+      				b=random.nextInt(90 - 1 + 1) + 1;
+				h=random.nextInt(90 - 1 + 1) + 1;
     			}
 			area = (b*h)/2;
 		}
@@ -46,8 +46,8 @@ public class Area {
 				System.out.println("Enter Breadth ");
 				b = sc.nextFloat();
     			} catch (Exception e) {
-      				b=7;
-				l=9;
+      				b=random.nextInt(90 - 1 + 1) + 1;
+				l=random.nextInt(90 - 1 + 1) + 1;
     			}
 			area = (b*l);
 		}
